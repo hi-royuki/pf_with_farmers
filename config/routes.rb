c7c80ff272c4dcb4e51100f1d1ff507221aab5ea
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-22 08:15:15 +0000

    [add]一般ユーザー

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :general_users
   devise_for :farmer_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
