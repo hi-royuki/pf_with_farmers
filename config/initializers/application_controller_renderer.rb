f1e9a3c81f09e94968cf4d5ed3b86c0948adce4e
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-24 14:56:44 +0000

    [Fix]画像エラー記述

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2341b443cae54b0fe1c8479bcff660c4a58ad758e9aefa8731ad54bea2801b4a21f3bed241e96ddf76b7386d0458f10d927f2598152124bb3b10618dd8a4f614'
