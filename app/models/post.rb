69dcd6fed4a43668c3dfded4c5e4e4a14775f734
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-24 15:19:11 +0000

    [Fix]バリデーションの実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   # farmeruserモデルを関連づける
   belongs_to :farmer_user
   attachment :image
+
+  # バリデーションの設定
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :introduction, presence: true
+
 end
