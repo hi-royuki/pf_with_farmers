64e96ee8be8cf961e3cc402afeaf1df95e10ac02
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-24 13:11:00 +0000

    [Fix]カラム設定

--- db/migrate/20211124040055_create_posts.rb
+++ db/migrate/20211124040055_create_posts.rb
@@ -2,6 +2,11 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
 
+      t.integer :farmer_user_id
+      t.text :title
+      t.string :image_id
+      t.text :introduction
+
       t.timestamps
     end
   end
