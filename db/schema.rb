25e048aea02113f5871daa2fac5ee9a7863fee11
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-24 13:11:15 +0000

    [Fix]カラム設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_081454) do
+ActiveRecord::Schema.define(version: 2021_11_24_040055) do
 
   create_table "farmer_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2021_11_22_081454) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_general_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_general_users_on_reset_password_token", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "farmer_user_id"
+    t.text "title"
+    t.string "image_id"
+    t.text "introduction"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
