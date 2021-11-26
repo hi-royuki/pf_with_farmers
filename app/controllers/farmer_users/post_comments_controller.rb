c581c43affc3540f80fb7137d67718d300004f7b
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-26 10:33:03 +0000

    [Fix]コメント反映されないエラー解消

--- app/controllers/farmer_users/post_comments_controller.rb
+++ app/controllers/farmer_users/post_comments_controller.rb
@@ -1,9 +1,11 @@
 class FarmerUsers::PostCommentsController < ApplicationController
   before_action :authenticate_farmer_user!
+
   def create
     post = Post.find(params[:post_id])
     post_comment = current_farmer_user.post_comments.new(post_comment_params)
     post_comment.post_id = post.id
+
     post_comment.save
     redirect_to farmer_users_post_path(params[:post_id])
   end
