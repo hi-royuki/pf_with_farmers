b21fcdbc6f288117ae5de76bed843bec336a173e
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-26 09:28:29 +0000

    [Fix]権限の設定

--- app/controllers/farmer_users/users_controller.rb
+++ app/controllers/farmer_users/users_controller.rb
@@ -1,7 +1,26 @@
 class FarmerUsers::UsersController < ApplicationController
-  def show
-    @user = current_farmer_user
-    @posts = @user.posts.all
+  before_action :authenticate_farmer_user!
 
+  def show
+    @farmer_user = current_farmer_user
+    @posts = @farmer_user.posts.all
   end
+
+  def edit
+    @farmer_user = current_farmer_user
+  end
+
+  def update
+    @farmer_user = current_farmer_user
+    if @farmer_user.update(farmer_user_params)
+      flash[:notice] = '情報を変更しました。'
+      redirect_to farmer_users_users_path(current_farmer_user)
+    else
+      render :edit
+    end
+  end
+
+   def farmer_user_params
+     	params.require(:farmer_user).permit(:name, :email, :profile_image)
+   end
 end
