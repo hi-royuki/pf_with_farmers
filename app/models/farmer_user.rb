b56da48cb18e32f3fbe05475c85523f1de8d48a3
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-26 09:28:46 +0000

    [Fix]

--- app/models/farmer_user.rb
+++ app/models/farmer_user.rb
@@ -11,5 +11,5 @@ class FarmerUser < ApplicationRecord
           has_many :post_comments, dependent: :destroy
 
           # refileを使って、画像投稿ができるようにする
-          attachment :profile_image
+           attachment :profile_image
 end
