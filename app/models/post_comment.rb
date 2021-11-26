43fa1dbe63e02519d5a3b0a929cb8afca1c5fc82
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-26 10:32:40 +0000

    [Fix]コメント反映されないエラー解消

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,7 +1,8 @@
 class PostComment < ApplicationRecord
-  
-  belongs_to :farmer_user
-  belongs_to :general_user
+
+  belongs_to :farmer_user, optional: true
+# Rails 5.1から関連付けが存在しない場合、belongs_toはデフォルトで検証エラーをトリガーします。
+  belongs_to :general_user, optional: true
   belongs_to :post
-  
+
 end
