01d9cee10029b3b50c94ddbefbfcc4202ea85407
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-28 15:47:56 +0000

    [Fix]コメント要修正必要

--- app/controllers/general_users/posts_controller.rb
+++ app/controllers/general_users/posts_controller.rb
@@ -1,11 +1,15 @@
 class GeneralUsers::PostsController < ApplicationController
+
   before_action :authenticate_general_user!
   def index
     @post = Post.all
   end
 
   def show
+
     @post = Post.find(params[:id])
+    # @post_comments = @post.comments
+    @post_comment = PostComment.new #コメントのインスタンス変数を記述
   end
 
 end
