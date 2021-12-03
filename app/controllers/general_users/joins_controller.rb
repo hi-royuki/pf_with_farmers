516530c540f62cbe21542a14500874c390eb5443
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-12-03 12:26:30 +0000

    [Fix]

--- app/controllers/general_users/joins_controller.rb
+++ app/controllers/general_users/joins_controller.rb
@@ -1,26 +1,49 @@
 class GeneralUsers::JoinsController < ApplicationController
 
   def new
+    @event = Event.find(params[:event_id])
     @join = Join.new
   end
 
-  def index
+   # newアクションから入力内容を受け取り、
+   # 送信ボタンを押されたらcreateアクションを実行します
+  def confirm
+    @event = Event.find(params[:event_id])
+    @join = Join.new(join_params)
+    @join.general_user_id = current_general_user.id
   end
 
-  def show
+  # 入力内容に誤りがあった場合
+  def back
+    @event = Event.find(params[:event_id])
+    @join = Join.new(join_params)
+    render :new
   end
 
   def create
-  end
-
-  def confirm
-    @join = Join.new(join_params)
-    
-    
-    
+     @event = Event.find(params[:event_id])
+     @join = current_general_user.joins.new(join_params)
+    if @join.save
+     redirect_to complete_general_users_event_joins_path
+    else
+     render :new
+    end
   end
 
   def complete
+  end
+
+  def index
+    @general_user = current_general_user
+    @event = Event.find(params[:event_id])
+    @joins = @general_user.joins.all
+  end
+
+
+  private
+
+  def join_params
+    params.require(:join).permit(:general_user_id, :email, :phone, :people, :name, :memo)
   end
 
 end
