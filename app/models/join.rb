0dac067e26e6f6be9861bb456237debbe3e7fc6f
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-12-03 12:24:54 +0000

    [Fix]バリデーション記述

--- app/models/join.rb
+++ app/models/join.rb
@@ -1,11 +1,11 @@
 class Join < ApplicationRecord
 
-  belongs_to :farmer_user
-  belongs_to :general_user
-  belongs_to :event
+  belongs_to :farmer_user, optional: true
+  belongs_to :general_user, optional: true
+  belongs_to :event, optional: true
 
    validates :email, presence: true
    validates :people, presence: true
    validates :name, presence: true
-   validates :price, presence: true
+   validates :phone, presence: true
 end
