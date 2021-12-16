7fc867589da1c89abde97ba8df5cc7c1bc86b60d
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-12-16 16:29:21 +0000

    [Fix]権限付与

--- app/controllers/farmer_users/customers_controller.rb
+++ app/controllers/farmer_users/customers_controller.rb
@@ -1,5 +1,5 @@
 class FarmerUsers::CustomersController < ApplicationController
-
+before_action :authenticate_farmer_user!
   def show
     @farmer_user = FarmerUser.find(params[:id])
     @posts = @farmer_user.posts.all.order(created_at: :desc)
