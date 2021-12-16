4f994f00e2a24ce7eb05f036147fde7d1099072a
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-12-16 15:59:47 +0000

    [Fix]パス修正

--- app/controllers/general_users/customers_controller.rb
+++ app/controllers/general_users/customers_controller.rb
@@ -2,7 +2,8 @@ class GeneralUsers::CustomersController < ApplicationController
 
   def show
     @farmer_user = FarmerUser.find(params[:id])
-    @posts = @farmer_user.posts.all
+    @posts = @farmer_user.posts.all.order(created_at: :desc)
+    @events = @farmer_user.events.all.order(created_at: :desc)
   end
 
 end
