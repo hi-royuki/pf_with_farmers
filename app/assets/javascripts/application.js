93145d8f111906307135b8ad8e64cdbf013721d7
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-12-22 12:34:58 +0000

    [Fix]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,4 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 
-//= require rails-ujs
-//= require activestorage
-
-//= require_tree .
